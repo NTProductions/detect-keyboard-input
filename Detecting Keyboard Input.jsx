6b27f91640216cd825b85e9ab7e90d4da07a307d
Nate Lovell <Natelovell99@gmail.com>
2020-05-30 00:08:41 -0600

    Create Detecting Keyboard Input.jsx

---
+++ Detecting Keyboard Input.jsx
@@ -0,0 +1,46 @@
+﻿var window = new Window("palette", "", undefined);
+var text = window.add("statictext", undefined, "Detect keyboard input");
+
+window.center();
+window.show();
+
+var inUI = false;
+
+function inside() {
+        inUI = true;
+    }
+
+function outside() {
+        inUI = false;
+    }
+
+window.addEventListener('mouseover', inside);
+window.addEventListener('mouseout', outside);
+window.addEventListener('keydown', alertKey);
+
+function alertKey() {
+        var myKeyState = ScriptUI.environment.keyboardState;
+        
+        var pressedString = myKeyState.keyName;
+        
+        if(myKeyState.altKey) {
+            pressedString = "Alt + " + pressedString;
+            }
+        
+        if(myKeyState.ctrlKey) {
+            pressedString = "Ctrl + " + pressedString;
+            }
+        
+        if(myKeyState.shiftKey) {
+            pressedString = "Shift + " + pressedString;
+            }
+        
+        if(myKeyState.keyName == "F" && myKeyState.shiftKey == true) {
+            alert("You pressed Shift + F");
+            } 
+        
+        if(myKeyState.keyName) {
+        alert("You pressed " + pressedString);
+        }
+        
+    }
